2f1ce5cc6dd6f39c8187959aba93816f338ac2fe
Name <user@example.com>
2024-12-30 23:48:03 +0545

    starting

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const navbarData = [
+  { title: "Home", href: "/" },
+  { title: "Blood Available", href: "/bloodavailable" },
+  { title: "Donate Blood", href: "/donateblood" },
+  { title: "Notification", href: "/notification" },
+  { title: "Login", href: "/login" },
+];
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
